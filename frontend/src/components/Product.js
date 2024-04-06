8f7ec76a407c1369aaef7a846bf737e2224d11b3
ItsMeNozo <dungtuyetngoc@gmail.com>
2024-04-06 13:17:06 +0700

    fix create product

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from './Rating';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 export default function Product(props) {
   const { product } = props;
@@ -13,16 +13,11 @@ export default function Product(props) {
         <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </Link>
-        <Rating
-          rating={product.rating}
-          numReviews={product.numReviews}
-        ></Rating>
+        <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
         <div className="row">
           <div className="price">${product.price}</div>
           <div>
-            <Link to={`/seller/${product.seller._id}`}>
-              {product.seller.seller.name}
-            </Link>
+            <Link to={`/seller/${product.seller._id}`}>{product.seller.name}</Link>
           </div>
         </div>
       </div>
